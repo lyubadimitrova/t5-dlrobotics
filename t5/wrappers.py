acdeaf48cc96a296d3c53b51ce17557421de4476
Lyuba Dimitrova <lyuba.dimitrova@abv.bg>
2021-02-26 22:13:46 +0100

    cleans up and changes reward

--- t5/wrappers.py
+++ t5/wrappers.py
@@ -1,90 +1,71 @@
 import gym
 import numpy as np
 
-class CmaReacherWrapper(gym.Wrapper):
 
-    def __init__(self, env, params={}):
+class CmaReacherWrapper(gym.Wrapper):
+    def __init__(self, env, steps_before_reward=40):
 
         super(CmaReacherWrapper, self).__init__(env)
 
         self.weight_matrix_scale = 10       # (optional?)
+        self.steps_before_reward = steps_before_reward
+        self.reward_weight = steps_before_reward
 
         self.time_limit = 1
 
-        self.start_pos = self.init_qpos[:2]
-        self.start_vel = self.init_qvel[:2]
-
-        self.qvels = [self.init_qvel]
-        self.qposs = [self.init_qpos]
-        self.xvelps = []
-        self.xvelrs = []
-        self.actions_taken = []
+        self.qvels = [self.start_vel]
+        self.qposs = [self.start_pos]
 
         self._steps = 0
         # print('frame skip: ', self.env.frame_skip)
 
-    def step(self, vel):
-        
-        # self.actions_taken.append(vel)
-        # print(vel)
-        # print('jv ', self.joint_vels)
-        old_vel = self.joint_vels
-        acc = (vel - old_vel) #/ 200
+    def step(self, a):
 
-        self.xvelrs.append(old_vel)
-        reward = self._reward(vel, acc)
         self._steps += 1
 
-        # normalize action
-        # acc = np.tanh(acc)
-        self.do_simulation(acc, self.frame_skip)
+        reward = self._reward(a)
+
+        self.do_simulation(a, self.frame_skip)
 
         done = self._steps * self.dt > self.time_limit
 
-        self.qvels.append(self._qvel)
-        self.qposs.append(self._qpos)
+        self.qvels.append(self.current_vel)
+        self.qposs.append(self.current_pos)
 
-        # print(self._steps)
         return self._get_obs().copy(), reward, done, None
 
-    def _reward(self, vel, acc):
-        reward = 0
-
-        if self._steps == 40:  # max episode length for Reacher-v2 is 50
-            reward = np.linalg.norm(self._dist_to_goal())  # dist reward
-
+    def _reward(self, a):
+        reward = 0.0
+        if self._steps == self.steps_before_reward:
+            reward -= np.linalg.norm(self._dist_to_goal())
+            reward -= np.linalg.norm(self.current_vel)
         reward = - reward ** 2
-        # reward -= np.square(vel).sum()
-        # reward -= 1e-6 * np.sum(acc**2)  # penalize high accelerations
-        reward -= 1e-6 * np.square(acc).sum()
-        if self._steps == 40:
-            reward -= 0.1 * np.sum(vel**2) ** 2  # fingertip not moving too fast
+        reward -= 1e-3 * np.square(a).sum()
 
         return reward
 
     @property
-    def _qvel(self):
-        return self.sim.data.qvel.flat[:]
+    def start_pos(self):
+        return self.init_qpos[:2].copy()
 
     @property
-    def _qpos(self):
-        return self.sim.data.qpos.flat[:]
+    def start_vel(self):
+        return self.init_qvel[:2].copy()
 
     @property
-    def joint_vels(self):
-        joint0 = self.sim.data.get_body_xvelr('body0')[-1]  # z-axis rotational velocity
-        joint1 = self.sim.data.get_body_xvelr('body1')[-1]
-        return np.array([joint0, joint1])
+    def current_pos(self):
+        return self.sim.data.qpos.flat[:2].copy()
+
+    @property
+    def current_vel(self):
+        return self.sim.data.qvel.flat[:2].copy()
 
     def _dist_to_goal(self):
         return self.get_body_com("fingertip") - self.get_body_com("target")
 
     def reset(self):
-        self.qvels = [self.init_qvel]
-        self.qposs = [self.init_qpos]
-        self.actions_taken = []
-        self.xvelrs = []
-        self.xvelps = []
+        self.qvels = [self.start_vel]
+        self.qposs = [self.start_pos]
 
         self._steps = 0
         qpos = self.init_qpos  # np.array([0., 0., 0.1, -0.1])
@@ -98,10 +79,10 @@ class CmaReacherWrapper(gym.Wrapper):
         return np.concatenate([
           np.cos(theta),
           np.sin(theta),
-          # self.get_body_com("target"),
           self.sim.data.qpos.flat[2:],
           self.sim.data.qvel.flat[:2],
           self.get_body_com("fingertip") - self.get_body_com("target"),
           [self._steps]
         ])
+
 
