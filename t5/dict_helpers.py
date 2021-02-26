6f70370cabfd7570bf4a860e911d1e0d19bf4e1f
Lyuba Dimitrova <lyuba.dimitrova@abv.bg>
2021-02-26 22:10:41 +0100

    references PPO wrapper instead of sb3 PPO

--- t5/dict_helpers.py
+++ t5/dict_helpers.py
@@ -1,8 +1,7 @@
-import stable_baselines3
 from alr_envs.utils.dmp_async_vec_env import DmpAsyncVectorEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
 import t5.algorithm
 
-ALGORITHMS = {'ppo': stable_baselines3.PPO, 'cma': t5.algorithm.CMA}
+ALGORITHMS = {'ppo': t5.algorithm.PPO, 'cma': t5.algorithm.CMA}
 
 VECTORIZERS = {'ppo': DummyVecEnv, 'cma': DmpAsyncVectorEnv}
