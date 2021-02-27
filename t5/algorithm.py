c2a7cf2bd497323019267dd4b0e44d3489820b71
Lyuba Dimitrova <lyuba.dimitrova@abv.bg>
2021-02-27 12:34:27 +0100

    moves algo-specific functions from Experiment class

--- t5/algorithm.py
+++ t5/algorithm.py
@@ -1,10 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import stable_baselines3 as sb3
+from stable_baselines3.common.evaluation import evaluate_policy
 from cma import CMAEvolutionStrategy
 
 
-class CMA:
+class Algo:
+    def learn(self, total_timesteps):
+        raise NotImplementedError
+
+    def save_model(self, path):
+        raise NotImplementedError
+
+    def load_model(self, path):
+        raise NotImplementedError
+
+    def evaluate_model(self, env):
+        raise NotImplementedError
+
+
+class CMA(Algo):
 
     def __init__(self, env, hyperparams):
 
@@ -56,9 +71,17 @@ class CMA:
 
         return x_start
 
+    def load_model(self, path):
+        self.algo.mean = np.load(path / 'algo_mean.npy')
+        self.opts = np.load(path / 'opts.npy')
+
     def save_model(self, path):
         np.save(path / 'algo_mean.npy', self.algo.mean)
         np.save(path / 'opts.npy', self.opts)
+
+    def evaluate_model(self, test_env):
+        reward, _ = test_env.rollout(self.algo.mean, render=True)
+        return reward
 
     def plot_curve(self, max_value=None, min_value=None, save=None):
         plt.figure()
@@ -79,7 +102,7 @@ class CMA:
             plt.savefig(save)
 
 
-class PPO:
+class PPO(Algo):
 
     def __init__(self, env, hyperparams):
         self.env = env
@@ -88,5 +111,13 @@ class PPO:
     def learn(self, total_timesteps):
         self.algo.learn(total_timesteps=total_timesteps)
 
+    def load_model(self, path):
+        self.algo = sb3.PPO.load(path / 'model')
+
     def save_model(self, path):
         self.algo.save(path / 'model')
+
+    def evaluate_model(self, test_env):
+        reward, _ = evaluate_policy(self.algo, test_env,
+                                    n_eval_episodes=10, render=True, deterministic=True, warn=False)
+        return reward
--- t5/experiment.py
+++ t5/experiment.py
@@ -6,23 +6,24 @@ from pathlib import Path
 import shutil
 
 import t5.dict_helpers as dict_helpers
-from stable_baselines3.common.evaluation import evaluate_policy
+from t5.algorithm import CMA, PPO
+
 from stable_baselines3.common.vec_env import VecNormalize
 
 
 class Experiment:
 
     def __init__(self, config_path):
+        self.cfg_path = config_path
         self.cfg = load_config(config_path)  # self.cfg is a multilevel dictionary
-        self.save_dir = make_experiment_dir(self.cfg)
-        shutil.copyfile(config_path, self.save_dir / 'config.yml')
+        self.model_dir = None
 
         self.total_timesteps = self.cfg['n_timesteps']
 
         algo = self.cfg['algorithm'].lower()
-        assert algo in {'ppo', 'cma'}
+        assert algo in {'ppo', 'cma'}, "Algorithm {} not supported.".format(algo)
 
-        self.algo = dict_helpers.ALGORITHMS[algo]
+        self.algo_class = dict_helpers.ALGORITHMS[algo]
 
         self.env_name = self.cfg['env']
         self.env_params = self.cfg['env_params']
@@ -47,35 +48,33 @@ class Experiment:
 
         self.build_vec_wrappers()
 
-        self.model = self.algo(env=self.env, hyperparams=self.cfg['algo_params'])
+        self.model = self.algo_class(env=self.env, hyperparams=self.cfg['algo_params'])
 
         # self.eval_function =
 
         # TODO: logging (path & tensorboard)
 
+    def load(self, model_path):
+        self.model_dir = Path(model_path)
+        self.model.load_model(self.model_dir)
+
     def run(self):
         self.model.learn(self.total_timesteps)
-        self.env.close()
+        # self.env.close()
 
     def save(self):
-        self.model.save_model(self.save_dir)
+        self.model_dir = make_experiment_dir(self.cfg)
+        shutil.copyfile(self.cfg_path, self.model_dir / 'config.yml')
+
+        self.model.save_model(self.model_dir)
         if isinstance(self.env, VecNormalize):
-            self.env.save(self.save_dir / 'vecnormalize.pkl')
+            self.env.save(self.model_dir / 'vecnormalize.pkl')
 
     def test_learned(self):
-        test_env = make_env(self.build_wrap_env)()
-        if hasattr(test_env, 'rollout'):
-            reward, _ = test_env.rollout(self.model.algo.mean, render=True)
-        else:
-            test_env = make_env(self.build_wrap_env, vectorizer=self.vectorizer)
-            test_env = VecNormalize.load(self.save_dir / "vecnormalize.pkl", test_env)
-            test_env.training = False
-            test_env.norm_reward = False
-            reward, _ = evaluate_policy(self.model.algo, test_env,
-                                        n_eval_episodes=10, render=True, deterministic=True, warn=False)
-
-        print('Episode reward: ', reward)
+        test_env = make_test_env(self.model, self.build_wrap_env, self.vectorizer, self.model_dir)
+        reward = self.model.evaluate_model(test_env)
         test_env.close()
+        print('Episode reward: ', reward)
         return reward
 
     def build_wrap_env(self):
@@ -92,7 +91,6 @@ class Experiment:
                 self.built_wrapper_params.append(parse_to_dict(self.wrapper_params[i]))
 
     def build_vec_wrappers(self):
-
         for i in range(len(self.built_wrappers)):
             if 'vec' in self.wrappers[i].lower():  # only vec wrappers
                 self.env = self.built_wrappers[i](self.env, self.built_wrapper_params[i])
@@ -111,6 +109,20 @@ def make_env(creator, n_envs=1, hyperparams={}, vectorizer=None, base_seed=0):
         return maker(0)
 
     return vectorizer(env_fns=[maker(i) for i in range(n_envs)], **hyperparams)
+
+
+def make_test_env(model, creator, vectorizer=None, model_dir=None):
+    if isinstance(model, CMA):
+        return make_env(creator)()
+    elif isinstance(model, PPO):
+        test_env = make_env(creator, vectorizer=vectorizer)
+        try:
+            test_env = VecNormalize.load(model_dir / "vecnormalize.pkl", test_env)
+            test_env.training = False
+            test_env.norm_reward = False
+        except FileNotFoundError:
+            pass
+        return test_env
 
 
 def parse_params(params_dict):
