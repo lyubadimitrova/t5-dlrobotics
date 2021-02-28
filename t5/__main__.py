310acae4861073dc429e4faa60a4eacf6b76dacd
Lyuba Dimitrova <lyuba.dimitrova@abv.bg>
2021-02-28 10:56:46 +0100

    removes redundant config_file option from CLI for testing

--- t5/__main__.py
+++ t5/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from t5.experiment import Experiment
 
@@ -6,17 +7,23 @@ from t5.experiment import Experiment
 def main():
     ap = argparse.ArgumentParser("Episodic vs Step-based Framework")
 
-    ap.add_argument("config_path", type=str,
+    ap.add_argument("-c", "--config_file", type=str,
                     help="path to config file (specifies policy representation and policy search algorithm\
                     , with relevant hyperparameters)")
 
-    ap.add_argument("--model_path", type=str,
+    ap.add_argument("-m", "--model_path", type=str,
                     help="if given, evaluates a trained agent instead of training it")
 
     args = ap.parse_args()
 
+    if not args.config_file and not args.model_path:
+        ap.error('Please specify a config file (-c) to train or a model path (-m) to test.')
+
+    if args.model_path:  # overwrite is model_path is given
+        args.config_file = os.path.join(args.model_path, 'config.yml')
+
     # configure experiment
-    exp = Experiment(args.config_path)
+    exp = Experiment(args.config_file)
 
     if args.model_path:
         exp.load(args.model_path)
